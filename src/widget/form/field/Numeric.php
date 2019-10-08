cb53b8b3723345586dcf1a20415508f4d7125003
DaviMenezes <davimenezes.dev@gmail.com>
2019-10-08 11:50:15 -0300

    componente numerico agora extende o componente Varchar
    e recebeu uma atualização no construtor

--- src/widget/form/field/Numeric.php
+++ src/widget/form/field/Numeric.php
@@ -2,16 +2,13 @@
 
 namespace Dvi\Adianti\Componente\Model\Form\Fields;
 
-use Adianti\Base\Lib\Core\AdiantiCoreTranslator;
-use Adianti\Base\Lib\Widget\Base\TScript;
-use Adianti\Base\Lib\Widget\Form\TForm;
-use Adianti\Base\Lib\Widget\Form\TNumeric;
 use Dvi\Component\Widget\Form\Field\Contract\FormField;
 use Dvi\Component\Widget\Form\Field\FormFieldTrait as FormFieldTrait;
 use Dvi\Component\Widget\Form\Field\FormFieldValidationTrait;
 use Dvi\Component\Widget\Form\Field\SearchableField;
 use Dvi\Component\Widget\Form\Field\Type\FieldTypeFloat;
-use Exception;
+use Dvi\Component\Widget\Form\Field\Varchar;
+use Dvi\Support\View\View;
 
 /**
  * Fields Numeric
@@ -22,15 +19,26 @@ use Exception;
  * @copyright  Copyright (c) 2018. (davimenezes.dev@gmail.com)
  * @link https://github.com/DaviMenezes
  */
-class Numeric extends TNumeric implements FormField
+class Numeric extends Varchar implements FormField
 {
     use FormFieldTrait;
     use FormFieldValidationTrait;
     use SearchableField;
 
-    public function __construct($name, $decimals, $decimalsSeparator, $thousandSeparator, $required = false, string $label = null, bool $replaceOnPost = true)
-    {
-        parent::__construct($name, $decimals, $decimalsSeparator, $thousandSeparator, $replaceOnPost);
+    public function __construct(
+        string $name,
+        int $decimals,
+        string $decimalsSeparator,
+        string $thousandSeparator,
+        bool $required = false,
+        string $label = null,
+        bool $replaceOnPost = true
+    ) {
+        parent::__construct($name);
+        $this->tag->{'pattern'}   = '[0-9]*';
+        $this->tag->{'inputmode'} = 'numeric';
+
+        parent::setNumericMask($decimals, $decimalsSeparator, $thousandSeparator, $replaceOnPost);
 
         $this->setup($label ?? $name, $required);
 
