fc5dfe08ddc6650cc047140c826be5a4802c8b44
DaviMenezes <davimenezes.dev@gmail.com>
2019-10-11 13:26:47 -0300

    text component view

--- src/widget/form/field/Text/View/text.blade.php
+++ src/widget/form/field/Text/View/text.blade.php
@@ -1,3 +1,7 @@
-{!!$label!!}
-<textarea {{$properties}}>{{$value}}</textarea>
-{!! $field_info !!}
+{{--text component--}}
+{{--@author     Davi Menezes--}}
+{{--@copyright  Copyright (c) 2018. (davimenezes.dev@gmail.com)--}}
+{{--@see https://github.com/DaviMenezes--}}
+@include('Widget.Form.Field.Base.label')
+<textarea @include('Widget.Form.Field.Base.properties') >{{$value}}</textarea>
+@include('Widget.Form.Field.Base.field_info')
